e75b528428dd263c0fcf4712e5d67ba805c20f56
Exzender <42044185+Exzender@users.noreply.github.com>
2020-04-02 12:08:42 +0300

    fix channel post command parse

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const regex = /^\/([^@\s]+)@?(?:(\S+)|)\s?([\s\S]+)?$/i;
 
 /* eslint no-param-reassign: ["error", { "props": false }] */
 module.exports = () => mount('text', (ctx, next) => {
-  const parts = regex.exec(ctx.message.text.trim());
+  const messageText = ctx.updateType === 'channel_post' ? ctx.channelPost.text : ctx.message.text;
+  const parts = regex.exec(messageText);
   if (!parts) return next();
   const command = {
-    text: ctx.message.text,
+    text: messageText,
     command: parts[1],
     bot: parts[2],
     args: parts[3],
